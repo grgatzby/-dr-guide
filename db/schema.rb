517d94c6c211fde709c1f6db6776309a8867074c
Ilan Okker <ilan.okker@gmail.com>
2023-02-28 11:25:10 +0100

    setup fixed

--- db/schema.rb
+++ db/schema.rb
@@ -55,7 +55,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_27_173256) do
     t.datetime "updated_at", null: false
     t.string "first_name"
     t.string "last_name"
-    t.string "role"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
