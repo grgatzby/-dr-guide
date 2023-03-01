d4508f5d6c71ac306d546f634317d479c2b77a8f
Ilan Okker <ilan.okker@gmail.com>
2023-03-01 14:58:33 +0100

    peters fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_01_130412) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_01_130321) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -61,7 +61,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_01_130412) do
     t.datetime "updated_at", null: false
     t.string "description"
     t.bigint "user_id"
-    t.string "photo"
     t.index ["user_id"], name: "index_experiences_on_user_id"
   end
 
