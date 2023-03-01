949b0ed32a00281461f3223eaa7daaf2b127576a
Ian Kelly <idk13@hotmail.com>
2023-03-01 14:10:39 +0100

    added Review card genertor

--- db/schema.rb
+++ db/schema.rb
@@ -32,7 +32,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_28_224238) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "description"
-    t.string "photo"
     t.bigint "user_id"
     t.string "photo"
     t.index ["user_id"], name: "index_experiences_on_user_id"
