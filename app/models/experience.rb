58eb761694f91a2026ab7859969e2863f15ce0fa
Ilan Okker <ilan.okker@gmail.com>
2023-03-01 14:44:29 +0100

    image field added for new experience

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -1,6 +1,7 @@
 class Experience < ApplicationRecord
   has_many :reviews
   belongs_to :user
+  has_one_attached :photo
 
   validates :category, :title, :location, :description, presence: true
 end
--- db/migrate/20230228224238_add_photo_to_experiences.rb
+++
@@ -1,5 +0,0 @@
-class AddPhotoToExperiences < ActiveRecord::Migration[7.0]
-  def change
-    add_column :experiences, :photo, :string
-  end
-end
